d1fc2a5fe0227cf0190357db8e3027ee449812dd
Rishabh S. Jha <rishabhsbjha9379@gmail.com>
2025-08-11 21:53:08 +0530

    Add files via upload
    This Java program calculates the Simple Interest (SI) and Total Amount for a given loan or investment based on user input. It prompts the user to enter the principal amount, annual interest rate, and loan tenure in years. Using the formula:
    S.I.=P*R*T/100

---
+++ Loan_SimpleInterest_Calculation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Loan_SimpleInterest_Calculation {
+    public static void main(String[] args){
+System.out.println("Welcome,\nTo calculate the interest Please give the main data ");
+Scanner sc=new Scanner(System.in);
+System.out.print("Please Enter your principal amount=");
+double p=sc.nextDouble(); 
+System.out.print("Please Enter the rate of interest(in %)=");
+double i=sc.nextDouble();
+System.out.print("Please Enter your loan tenure (in years)=");
+double t=sc.nextDouble();
+double sum = (p*i*t)/100;
+System.out.print("This your Simple interest of your Loan=");
+System.out.println(sum);
+System.out.print("Your Total Amount is=");
+double R=(p+sum);
+System.out.println(R);
+System.out.println("Thank you \nJBMNMJ");
+
+    }
+}
+
